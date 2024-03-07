e4ba1bbeedacb9ed774edfd97405a296eb52a0d8
manuelcalerosolis <manuelcalerosolis@gmail.com>
2024-03-07 19:23:02 +0100

    document format

---
+++ aryentto_document_format/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Aryentto Document Format",
+    "summary": "Aryentto Document Format",
+    "version": "16.0.1.0.0",
+    "category": "Custom",
+    "author": "Xtendoo",
+    "license": "LGPL-3",
+    "application": True,
+    "depends": [
+        "base",
+        "point_of_sale",
+        "loyalty",
+    ],
+    "data": [
+        "data/report_paperformat.xml",
+        "report/loyalty_report.xml",
+        "report/loyalty_report_templates.xml",
+    ],
+    "installable": True,
+}
